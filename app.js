7064b55c939e6c6057dac6aadcb3fec157d3339b
frknceran <frknceran@gmail.com>
2020-09-17 20:48:39 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose  = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    if (req.method === 'OPTIONS'){
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+app.use(isAuth);
+
+app.use(
+    '/graphql', 
+    graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true  
+  })
+);
+ 
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+        process.env.MONGO_PASSWORD
+    }@cluster0-xn8a6.mongodb.net/${process.env.MONGO_DB
+        }?retryWrites=true&w=majority`
+).then(()=>{
+    app.listen(8000);
+})
+.catch(err => {
+    console.log(err);
+});
+
+/*
+const event = {
+    _id : Math.random().toString(),
+    title : args.eventInput.title,
+    description: args.eventInput.description,
+    price: +args.eventInput.price,
+    date: args.eventInput.date
+};
+*/
+
+/*
+app.get('/', (req, res, next) => {
+    res.send('Hello World!');   
+})
+*/
+
